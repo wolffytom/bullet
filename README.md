a1801ff93ab063e90f37682ab8e47e89ec9c6044
Jialiang Cui <jialing.cui@uisee.com>
2017-05-11 15:54:43 +0800

    modified 0511

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ## Install
 sudo apt-get install python-opencv
+
+## References
+
+# circle detection
+http://docs.opencv.org/2.4/modules/imgproc/doc/feature_detection.html?highlight=houghcircle#houghcircles
---
+++ targetdetect/showcircles.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+# ensure at least some circles were found
+if circles is not None:
+	# convert the (x, y) coordinates and radius of the circles to integers
+	circles = np.round(circles[0, :]).astype("int")
+ 
+	# loop over the (x, y) coordinates and radius of the circles
+	for (x, y, r) in circles:
+		# draw the circle in the output image, then draw a rectangle
+		# corresponding to the center of the circle
+		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+ 
+	# show the output image
+	cv2.imshow("output", np.hstack([image, output]))
+	cv2.waitKey(0)
