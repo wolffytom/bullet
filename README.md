ac407fdb11612519fdc9087e0f4bb73acb55175e
Jialiang Cui <jialing.cui@uisee.com>
2017-05-15 17:54:18 +0800

    commit 0513

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ sudo apt-get install python-opencv
 
 ## References
 
-# circle detection
+### circle detection
 http://docs.opencv.org/2.4/modules/imgproc/doc/feature_detection.html?highlight=houghcircle#houghcircles
+
+### haar
 https://rdmilligan.wordpress.com/2015/03/01/road-sign-detection-using-opencv-orb/
+https://pythonprogramming.net/haar-cascade-object-detection-python-opencv-tutorial/
---
+++ targetdetect/traincascade/download-image-by-link.py
@@ -0,0 +1,33 @@
+#import urllib.request
+import cv2
+import numpy as np
+import os
+import sys
+
+def store_raw_images():
+    #neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'   
+    #neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
+    furl = file('imgneturls.txt','r')
+
+    pic_num = 1
+    
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+        
+    for l in furl.readlines():
+        ll = l.split('\n')[0]
+        print l
+    for i in neg_image_urls.split('\n'):
+        try:
+            print(i)
+            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
+            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            # should be larger than samples / pos pic (so we can place our image on it)
+            resized_image = cv2.resize(img, (100, 100))
+            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
+            pic_num += 1
+            
+        except Exception as e:
+            print(str(e))  
+
+store_raw_images()
